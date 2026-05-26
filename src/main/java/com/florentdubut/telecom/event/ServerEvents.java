b5d77a6d980df11437d43c45e3203cf032f20563
Florent <florent.dubut@gmail.com>
2026-05-26 19:00:48 +0200

    Revert automatic recalculation on connection which caused network wiping

--- src/main/java/com/florentdubut/telecom/event/ServerEvents.java
+++ src/main/java/com/florentdubut/telecom/event/ServerEvents.java
@@ -4,18 +4,10 @@ import com.florentdubut.telecom.TelecomMod;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 
 @EventBusSubscriber(modid = TelecomMod.MODID, bus = EventBusSubscriber.Bus.GAME)
 public class ServerEvents {
     private static int tickCounter = 0;
-
-    @SubscribeEvent
-    public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        if (!event.getEntity().level().isClientSide() && event.getEntity() instanceof net.minecraft.server.level.ServerPlayer player) {
-            com.florentdubut.telecom.network.NetworkTracer.scheduleRecalculation(player.serverLevel());
-        }
-    }
 
     @SubscribeEvent
     public static void onServerTick(ServerTickEvent.Post event) {
